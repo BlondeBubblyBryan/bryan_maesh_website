c38b32811dc68aff5dba7079c03e484104679fa5
mmmikkel <michael@duyvesteijn.net>
2019-08-05 13:22:24 +0800

    Adding files

---
+++ maesh_env/main/settings/_servers.py
@@ -0,0 +1,11 @@
+import socket
+
+def get_server_type():
+    host_name = socket.gethostname()
+    server_type = 'Server-type-Not-defined-or-found'
+    if host_name.startswith('RD000'):
+        # server_type = 'production'
+        server_type = 'test_production'
+    elif host_name.startswith('Mikkel'):
+        server_type = 'development'
+    return server_type
---
+++ maesh_env/main/settings/production.py
@@ -0,0 +1,35 @@
+from ._base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+X_FRAME_OPTIONS = 'DENY'
+
+ALLOWED_HOSTS = ['payments.maesh.io']
+
+SITE = 'http://payments.maesh.io:8000/'
+
+# To only have cookies to be sent over https
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flowerflower',
+        'USER': 'nzbt_db@nzbt-db',
+        'PASSWORD': 'Spring123.',
+        'HOST': 'nzbt-db.postgres.database.azure.com',
+        'PORT': '5432',
+    }
+}
+
+#BACKUP FOR DATABASE?
