8bbe5411fac4915640d50383100de9a8e61695c3
mmmikkel <michael@duyvesteijn.net>
2019-08-05 13:27:59 +0800

    Changed server setting

--- maesh_env/main/settings/_servers.py
+++ maesh_env/main/settings/_servers.py
@@ -3,9 +3,9 @@ import socket
 def get_server_type():
     host_name = socket.gethostname()
     server_type = 'Server-type-Not-defined-or-found'
-    if host_name.startswith('RD000'):
-        # server_type = 'production'
-        server_type = 'test_production'
+    if host_name.startswith('maesh'):
+        server_type = 'production'
+        #server_type = 'test_production'
     elif host_name.startswith('Mikkel'):
         server_type = 'development'
     return server_type
