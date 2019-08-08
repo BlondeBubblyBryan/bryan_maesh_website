2a77e56a5b8189cd6f9acc300e89a508ea2537c4
mmmikkel <michael@duyvesteijn.net>
2019-08-08 13:31:04 +0800

    Finalizing qr download now

--- maesh_env/payment/qr/qrcodegen.py
+++ maesh_env/payment/qr/qrcodegen.py
@@ -267,7 +267,7 @@ class QrCode(object):
 					parts.append("M{},{}h1v1h-1z".format(x + border, y + border))
 		return """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg style="width:200px;" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 {0} {0}" stroke="none">
+<svg id="qrsvg" width="200px" height="200px"  xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 {0} {0}" stroke="none">
 	<rect width="100%" height="100%" fill="#FFFFFF"/>
 	<path d="{1}" fill="#000000"/>
 </svg>
--- maesh_env/payment/static/maesh/js/script.js
+++ maesh_env/payment/static/maesh/js/script.js
@@ -1,14 +1,57 @@
-function launchiOSApp(url) {
+// // Initializing
+// window.onload = function(){
+//   var dwn = document.getElementById('btndownload'),
+//       canvas = document.getElementById('canvas'),
+//       context = canvas.getContext('2d');
 
-     var appleAppStoreLink = 'https://itunes.apple.com/sg/app/dbs-paylah!/id878528688?mt=8';
+//   // Event handler for download
+//   dwn.onclick = function(){
+//     download(canvas, 'myimage.png');
+//   }
 
-     var now = new Date().valueOf();
+// }
 
-     setTimeout(function () {
+// /* Canvas Donwload */
+// function download(canvas, filename) {
+//   /// create an "off-screen" anchor tag
+//   var lnk = document.createElement('a'), e;
 
-          if (new Date().valueOf() - now > 500) return;
-     window.location = url;
+//   /// the key here is to set the download attribute of the a tag
+//   lnk.download = filename;
 
-     }, 100);
+//   /// convert canvas content to data-uri for link. When download
+//   /// attribute is set the content pointed to by link will be
+//   /// pushed as "download" in HTML5 capable browsers
+//   lnk.href = canvas.toDataURL("image/png;base64");
 
-}
+//   /// create a "fake" click-event to trigger the download
+//   if (document.createEvent) {
+//     e = document.createEvent("MouseEvents");
+//     e.initMouseEvent("click", true, true, window,
+//                      0, 0, 0, 0, 0, false, false, false,
+//                      false, 0, null);
+
+//     lnk.dispatchEvent(e);
+//   } else if (lnk.fireEvent) {
+//     lnk.fireEvent("onclick");
+//   }
+// 
+
+ $(document).ready(function () {
+   $("#btn-downloadQR").click(function(){
+     downloadImage();
+   });
+ });
+
+ function downloadImage(){
+   html2canvas(document.querySelector("#yes"), {
+    windowWidth: document.querySelector("#yes").scrollWidth,
+    windowHeight: document.querySelector("#yes").scrollHeight
+}).then(canvas => {
+    a = document.createElement('a'); 
+    document.body.appendChild(a); 
+    a.download = "test.png"; 
+    a.href =  canvas.toDataURL();
+    a.click();
+  });  
+ }
