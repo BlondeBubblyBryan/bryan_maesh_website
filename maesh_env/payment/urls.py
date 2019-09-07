af89f90a6bf3f0ccc5073c40893799aebdac5c3a
mmmikkel <michael@duyvesteijn.net>
2019-09-07 21:04:48 +0800

    Including CORS to true for all

--- maesh_env/payment/urls.py
+++ maesh_env/payment/urls.py
@@ -47,7 +47,7 @@ if settings.LOCAL_DEV:
 	])
 else:
 	urlpatterns.extend([
-		path('', RedirectView.as_view(url='https://maesh.io', permanent=True), name='maesh_home_page'),
+		# path('', RedirectView.as_view(url='https://maesh.io', permanent=True), name='maesh_home_page'),
 		#GraphQL
     	path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=False))), #In production you'd set this to false
 	])
