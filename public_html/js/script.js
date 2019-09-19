72bd6cd6aec7d08b1843cba023c7e3e5982bb9c1
mmmikkel <michael@duyvesteijn.net>
2019-09-19 20:29:37 +0800

    Finalizing landing page for now

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,9 +1,13 @@
 $("#desktop").click(function(){
  	$(".on-desktop").show();
 	$(".on-mobile").hide();
+	$(this).css({'background-color': '#5d3edc', 'color': '#fff'});
+	$("#mobile").css({'background-color': '#fff', 'color': '#000'});
 });
 
 $("#mobile").click(function(){
  	$(".on-desktop").hide();
 	$(".on-mobile").show();
+	$(this).css({'background-color': '#5d3edc', 'color': '#fff'});
+	$("#desktop").css({'background-color': '#fff', 'color': '#000'});
 });
