937e3b12147c3af918abdb2c318ad244318faaca
mmmikkel <michael@duyvesteijn.net>
2019-09-19 13:49:41 +0800

    Adding images

---
+++ public_html/js/script.js
@@ -0,0 +1,9 @@
+$("#desktop").click(function(){
+ 	$(".on-desktop").show();
+	$(".on-mobile").hide();
+});
+
+$("#mobile").click(function(){
+ 	$(".on-desktop").hide();
+	$(".on-mobile").show();
+});
