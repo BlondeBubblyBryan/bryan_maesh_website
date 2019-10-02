001fd419a368bfeb8c7d227d38351b3fad5a8e4a
mmmikkel <michael@duyvesteijn.net>
2019-10-02 14:57:36 +0800

    Separated the payments code base from the website code base

--- maesh_env/payment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Transaction
-
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('id','company_name','paid','transaction_id','reference_code','amount', 'currency', 'UEN')
-
-admin.site.register(Transaction, TransactionAdmin)
--- maesh_env/payment/static/i_love_lamp/js/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#paynow").click(function(){
-		$("#paynow").toggleClass("color-select");
-		$("#select").toggleClass("disabled");
-		$("#circle").toggleClass("far");
-		$("#circle").toggleClass("fas");
-	});
-});
