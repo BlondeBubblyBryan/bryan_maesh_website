3ad3630e918cb554fb3a632558170f141edad0e7
aditya-67 <adityagannavarapu.67@gmail.com>
2020-07-01 11:32:28 +0530

    Mobile redirect tab

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,59 @@
-$("#desktop").click(function(){
- 	$(".on-desktop").show();
-	$(".on-mobile").hide();
-	$(this).css({'background-color': '#5d3edc', 'color': '#fff'});
-	$("#mobile").css({'background-color': '#fff', 'color': '#000'});
+$("#desktop").click(function () {
+  $(".on-desktop").show();
+  $(".on-mobile-qr").hide();
+  $(".on-mobile-redirect").hide();
+  $(this).css({
+    'background-color': '#5d3edc',
+    'color': '#fff'
+  });
+  $("#mobile-qr").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
+  $("#mobile-redirect").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
 });
 
-$("#mobile").click(function(){
- 	$(".on-desktop").hide();
-	$(".on-mobile").show();
-	$(this).css({'background-color': '#5d3edc', 'color': '#fff'});
-	$("#desktop").css({'background-color': '#fff', 'color': '#000'});
+$("#mobile-qr").click(function () {
+  $(".on-desktop").hide();
+  $(".on-mobile-qr").show();
+  $(".on-mobile-redirect").hide();
+  $(this).css({
+    'background-color': '#5d3edc',
+    'color': '#fff'
+  });
+  $("#desktop").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
+  $("#mobile-redirect").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
 });
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
+$("#mobile-redirect").click(function () {
+  $(".on-desktop").hide();
+  $(".on-mobile-qr").hide();
+  $(".on-mobile-redirect").show();
+  $(this).css({
+    'background-color': '#5d3edc',
+    'color': '#fff'
+  });
+  $("#desktop").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
+  $("#mobile-qr").css({
+    'background-color': '#fff',
+    'color': '#000'
+  });
+});
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 //Initialize
@@ -26,10 +66,10 @@ var costSaved = document.getElementById("cost-saved");
 var margin = document.getElementById("margin");
 
 //Slider
-$("document").ready(function() {
+$("document").ready(function () {
   $(".slider").rangeslider();
 });
-$.fn.rangeslider = function(options) {
+$.fn.rangeslider = function (options) {
   var obj = this;
   var defautValue = obj.attr("value");
   obj.wrap("<span class='range-slider'></span>");
@@ -48,30 +88,30 @@ function updateSlider(passObj) {
   var amount = (value - min);
   var percentage = Math.round((value - min) * 100 / range);
   var nextObj = obj.next();
-  nextObj.find("span.bar-btn").css("left", percentage-3 + "%");
+  nextObj.find("span.bar-btn").css("left", percentage - 3 + "%");
   nextObj.find("span.bar > span").css("width", percentage + "%");
-  nextObj.find("span.bar-btn > span").text("S$"+amount);
+  nextObj.find("span.bar-btn > span").text("S$" + amount);
 
   stripeCost.innerHTML = calculateCost(value, $("#stripe-percentage").html(), $("#stripe-fixed").html());
   maeshCost.innerHTML = calculateCost(value, $("#maesh-percentage").html(), $("#maesh-fixed").html());
   costSaved.innerHTML = (stripeCost.innerHTML - maeshCost.innerHTML).toFixed(2);
   margin.innerHTML = ((costSaved.innerHTML / value) * 100).toFixed(1);
   if (!(isFinite(margin.innerHTML))) {
-  	margin.innerHTML = "-";
+    margin.innerHTML = "-";
   } else {
-  	margin.innerHTML+="%";
+    margin.innerHTML += "%";
   }
 };
 
 //Calculate the transaction fees
 function calculateCost(orderValue, percentage, fixed) {
-	return (orderValue * Number(percentage)/100 + Number(fixed)).toFixed(2);
+  return (orderValue * Number(percentage) / 100 + Number(fixed)).toFixed(2);
 }
 
 
 document.querySelector('#prototype-video').playbackRate = 1.5;
 document.querySelector('#prototype-video').play();
 
-function navigatePluginPage(path){
-  window.location.href = './docs/'+path;
+function navigatePluginPage(path) {
+  window.location.href = './docs/' + path;
 }
